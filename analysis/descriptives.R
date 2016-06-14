8066a04231f6cbe1a24d50d0f75aa039b6f4d650
Thomas Klebel <tklebel@users.noreply.github.com>
2016-06-14 23:38:07 +0200

    conduct first descriptive analysis for factors

---
+++ analysis/descriptives.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(dplyr)
+library(purrr)
+library(fuehrerschein)
+library(tidyr)
+
+refactored <- readr::read_rds("data/refactored.rds")
+
+labels <- refactored %>%
+  attributes() %>%
+  .[c("names", "variable.labels")] %>%
+  bind_cols()
+
+# Descriptives
+
+# Alter -----
+refactored %>%
+  filter(!is.na(v3)) %>%
+  ggplot(aes(v3)) +
+  geom_bar() +
+  scale_x_discrete(drop = F) +
+  labs(title = find_label(labels, "v3"))
+
+
+# Map function over all factors ----
+refactored %>%
+  select(v3, v4, v8:v10, v12) %>%
+  plot_factors(.labels = labels)
+
